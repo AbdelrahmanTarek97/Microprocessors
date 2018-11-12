a371d52a1b67f5b60aab68650ac2879413b90bf4
Abdelrahman Tarek <Abdelrahman_tarek251@hotmail.com>
2018-11-12 14:38:05 +0200

    Fixing many bugs

--- UART.c
+++ UART.c
@@ -1,5 +1,4 @@
 #include<reg51.h>
-#include <stdio.h>
 #include <string.h>
 
 void uart_init();				//Initialize UART
@@ -10,38 +9,51 @@ void uart_msg(char *p);   			//String transmission
 void newLine();
 void wait4Letter(char x);
 void startTimer();
-//void endTimer();
+void endTimer();
+void trainUserX(bit user);
+void Timer0_ISR(void);
 
 int overFlows = 0x0000;
-
-void Timer0_ISR(void) interrupt 1 {
-	TR0 = 0;
-	//uart_msg("OverFlow Detected");
-	overFlows ++;
-	TF0 = 0;
-	TR0 = 1;
-	
-}
+unsigned long avg0[7] ; //store average for user 1
+unsigned long avg1[7] ;//store average for user 2
+unsigned char testingTimes[7]; //store testing time
+unsigned char password  [7] = "aaaaaaa";  //to store the password
+char test = '0';  // for now it's zero, but later on it will be from an input pin
+char user = '0';   //for now it's zero, but later on it will be from an input pin
 
 void main(void){
 	
-	unsigned char x ;
-	bit test = 0;  // for now it's zero, but later on it will be from an input pin
-	bit user = 0 ; // for now it's user zero
+	// Initialize UART settings
 	
 	uart_init();
 	
-	startTimer();
-	wait4Letter('a');
-	//endTimer();
-	
 	while(1){
-			if(!test){
 			
-				// CODE FOR TRAINING 
+			if(test == '0'){
+				
+				uart_msg("Now training  ");
+				
+				while(test == '0'){
+					
+					uart_msg("pick user 0 or 1 to train  ");
+					
+					user = uart_rx() ;
+					
+					if(user == '0'){
+						trainUserX(0);
+					}
+					else if(user == '1'){
+						trainUserX(1);
+					}
+					
+					uart_msg("press 0 to train again  or 1 to test   ");
+					test = uart_rx();
+					uart_tx(test);
+				}
+			
 			}
 			
-			if(test){
+			if(test== '1'){
 			
 				// CODE FOR TESTING
 			}
@@ -50,28 +62,86 @@ void main(void){
 	}
 }
 
+
+void trainUserX (bit user){
+	
+		unsigned char j;
+		unsigned char i;
+		// CODE FOR TRAINING 
+				
+		// FOR EACH TRIAL
+		for(j = '0'; j<'5' ; j++){
+			
+			uart_msg("trial number ");
+			uart_tx(j);
+			uart_msg("      ");
+							
+				// FOR EACH LETTER
+				for( i = 0; i<7;i++){
+								
+					startTimer();
+					wait4Letter(password[i]);
+					endTimer();
+								
+					if(!user){
+						avg0[i] += (overFlows << 16) | (TH0 << 8) | TL0;
+					}
+					else{
+						avg1[i] += (overFlows << 16) | (TH0 << 8) | TL0;
+					}
+					
+					overFlows = 0x00;
+				
+				}
+					
+		}
+				
+		for(i = 0 ; i<7 ; i++){
+			
+		avg0[i] /= 5 ;
+			
+		}
+}
+
 void wait4Letter(char x){
 
 	while(1){
 		char y = uart_rx();
-		uart_tx(y);
-			
+		
 		if(y == x){
-			uart_msg("recieved");
+			uart_msg("recieved   ");
 			uart_tx(x);
 			return;
 		}
 		else{
-			uart_msg("Wrong character !");
+			uart_msg("Wrong character !   ");
 		}
 	}
 }
 
+void uart_msg(char *p){
+	
+	while(*p != 0){
+		uart_tx(*p);
+		p++;
+	}
+	
+	return;
+	
+}
+
 void startTimer(){
 	TH0 = 0x00;
+	TL0 = 0x00;
 	TR0 = 1;
 	ET0 = 1;
 	EA = 1;
+}
+
+void endTimer(){
+	TR0 = 0;
+	ET0 = 0;
+	EA = 0;
 }
 
 void uart_init(){
@@ -80,7 +150,7 @@ void uart_init(){
 }
 
 void timer_init(){
-	TMOD = 0x22;
+	TMOD = 0x21;
 	TH1 = 0xFD;
 	TR1 = 1;
 }
@@ -102,14 +172,20 @@ char uart_rx(){
 	return x;
 }
 
-void uart_msg(char *p){
+void Timer0_ISR(void) interrupt 1 {
+	TR0 = 0;
+	//uart_msg("OverFlow Detected");
+	overFlows ++;
+	TH0 = 0x00;
+	TL0 = 0x00;
+	TF0 = 0;
+	TR0 = 1;
 	
-	while(*p != 0){
-		uart_tx(*p);
-		p++;
-	}
 }
+
+
 
 void newLine(){
 	uart_tx(0x0D);
 }
+
